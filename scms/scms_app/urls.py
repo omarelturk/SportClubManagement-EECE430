991299e9e3465284b4ce7e2d9079e31b8c65f38f
alimans587 <alimans587@gmail.com>
2022-04-11 01:50:52 +0300

    frontend almost ready for demo - missing teams only - work should be on backend now

--- scms/scms_app/urls.py
+++ scms/scms_app/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.Home, name='Home'),
+    path('About.html', views.About, name='About'),
+    path('Signin.html', views.Signin, name='Signin'),
+    path('Signup.html', views.Signup, name='Signup'),
+    path('StadiumMuseum.html', views.StadiumMuseum, name='StadiumMuseum'),
 ]
--- scms/scms_app/views.py
+++ scms/scms_app/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request, 'Home.html', {})
+
+
+def About(request):
+    return render(request, 'About.html', {})
+
+
+def Signin(request):
+    return render(request, 'Signin.html', {})
+
+
+def Signup(request):
+    return render(request, 'Signup.html', {})
+
+
+def StadiumMuseum(request):
+    return render(request, 'StadiumMuseum.html', {})
